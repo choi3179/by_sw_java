265c97b84ec26ae647fa3a0799c3500b639df048
choi3179 <jacobis@naver.com>
2023-11-20 17:26:05 +0900

    [ADD] 11.20 java 1일차 - 자료형 및 프로젝트 생성 테스트

---
+++ Info.java
@@ -0,0 +1,7 @@
+class Info{
+	public static void main(String[] args) {
+		System.out.println("최원규");
+		System.out.println("서울");
+		System.out.println("010-8200-7284");
+	}
+}
---
+++ day01_basic_02/src/ex01/basic/MainEntity.java
@@ -0,0 +1,25 @@
+package ex01.basic;
+
+public class MainEntity {
+    public static void main(String[] args) {
+        int su = 9;
+        char ch = 'A';
+        String str = "korea";
+        double d = 12.34;
+        float f = 12.34f;           // float은 f 접미사가 붙어야 컴파일러에서 float 타입으로 인식(double이 default)
+        long l = 100L;              // long은 L 접미사가 붙어야 Long 타입으로 인식(int가 default)
+
+        System.out.println(su);
+        System.out.println(ch);
+        System.out.println(str);
+        System.out.println(d);
+        System.out.println(f);
+        System.out.println(l);
+
+        int x = 2;      // 기본자료형
+        Integer y= 2;   // 참조형
+        x=y;
+        y=x;    // autoboxing / unboxing
+
+    }
+}
