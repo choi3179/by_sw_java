6117cfdeb017a6dd89fe41bc93e2d87bec737178
choi3179 <jacobis@naver.com>
2023-11-21 09:54:00 +0900

    [MOD] 11.21 패키지 rename

---
+++ java_lang_project/src/day1/ex01/basic/DataType.java
@@ -0,0 +1,11 @@
+package day1.ex01.basic;
+
+public class DataType {
+    public static void main(String[] args) {
+        short sh = -32768; // -32768 ~ 32767
+        int su = 32769;
+
+        su = sh; // 자동 형변환
+        sh = (short)su;  // 명시적 형변환 -> 데이터 손실 발생
+    }
+}
