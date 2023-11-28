5e4209dca993cdf82e7b26a7c69ffb6623e7f01e
choi3179 <jacobis@naver.com>
2023-11-28 09:43:00 +0900

    [ADD] 11.28 함수 오버로딩 학습 및 과제

---
+++ java_lang_project/src/day6/method/MethodQuiz04.java
@@ -0,0 +1,38 @@
+package day6.method;
+
+import java.util.Scanner;
+
+public class MethodQuiz04 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        plus(a,b);
+        System.out.println(a+" - "+b+" = "+ sub(a,b));
+        System.out.println(a+" * "+b+" = "+mul(a,b));
+        div(a,b);
+    }
+
+    public static void plus(int a,int b){
+        System.out.println(a+" + "+b+" = "+(a+b));
+    }
+    public static int sub(int a,int b){
+        return a-b;
+    }
+    public static int mul(int a,int b){
+        return a*b;
+    }
+    public static void div(int a,int b){
+       try{
+           int result = a/b;
+           System.out.println(a+" / "+b+" = "+ result);
+       }
+       catch(Exception e){
+           System.out.println("연산 오류 : " + e);
+       }
+    }
+}
