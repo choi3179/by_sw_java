f14c42e574a4b2164e3eb16945e8183b627ecc7c
choi3179 <jacobis@naver.com>
2023-12-06 09:06:28 +0900

    [ADD] 12.06 문제 풀이 추가

---
+++ java_lang_project/src/day12/hw/BJ1436.java
@@ -0,0 +1,24 @@
+package day12.hw;
+
+import java.util.Scanner;
+
+public class BJ1436 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int num = 0;
+        int cnt = 0;
+        while(true){
+            if(Integer.toString(num).contains("666")){
+                cnt++;
+                if(cnt == n)
+                    break;
+            }
+            num++;
+        }
+
+        System.out.println(num);
+    }
+}
