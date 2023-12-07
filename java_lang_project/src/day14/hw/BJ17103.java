7497153606152e882ddf69d2182cada5612fbfc2
choi3179 <jacobis@naver.com>
2023-12-07 17:51:42 +0900

    [MOD] 12.07 백준17103 푸는중

--- java_lang_project/src/day14/hw/BJ17103.java
+++ java_lang_project/src/day14/hw/BJ17103.java
@@ -12,9 +12,20 @@ public class BJ17103 {
         int t = Integer.parseInt(br.readLine());
         boolean[] sosu = new boolean[1000000/2];    // 입력 최대가 1,000,000 이고 실제 고려해야 할 소수는 입력의 절반 만큼이므로
 
-        for(int i=0;i<sosu.length;i++){
-            if(i==2) sosu[i] = true;
+        for(int i=1;i<sosu.length;i++)
+            sosu[i] = isPrime(i);
 
+        for(int i=0;i<t;i++){
+            int input = Integer.parseInt(br.readLine());
         }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) return false;
+        if (num <= 3) return true;
+        for(int i = 2; i <= num -1; i++) {
+            if(num % i == 0) return false;
+        }
+        return true;
     }
 }
